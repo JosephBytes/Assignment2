794a2b98ae234772a110f4bc3c5916e36aff2053
Joseph Abdulwahab <abduljoseph33@gmail.com>
2024-06-15 06:58:46 -0400

    fix: import data, cashier, and sandwich_maker in main

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-import data
-from sandwich_maker import SandwichMaker
-from cashier import Cashier
-
+import data 
+import cashier 
+import sandwich_maker
 
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = 
+cashier_instance = 
 
 
 
 
 def main():
-    ###  write the rest of the codes ###
+
 
 if __name__=="__main__":
     main()
