ede3882363b47ab24830846a49dc313fe07e8199
Joseph Abdulwahab <abduljoseph33@gmail.com>
2024-06-15 07:33:10 -0400

    feat: implemented the process_coins function

--- cashier.py
+++ cashier.py
@@ -5,7 +5,13 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert coins.")
+        dollars = int(input("how many ($1) dollars?: "))
+        half = int(input("how many half dollars?: ")) * 0.50
+        quarters = int(input("how many quarters?: ")) * 0.25
+        nickels = int(input("how many nickels?: ")) * 0.05
+        return dollars + half + quarters + nickels
+
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
